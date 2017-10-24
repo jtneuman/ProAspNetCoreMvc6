f7f4fa798ef0871f524b37c87eca405b4feda675
Jeff <jeff.neumann@phoenix.edu>
2017-10-24 07:51:02 -0400

    ProductController List method updated to take the ProductsListViewModel.

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -17,9 +18,24 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int page = 1) => View(repository.Products
-            .OrderBy(p => p.ProductID)
-            .Skip((page -1) * PageSize)
-            .Take(PageSize));
+        //public ViewResult List(int page = 1) => View(repository.Products
+        //    .OrderBy(p => p.ProductID)
+        //    .Skip((page -1) * PageSize)
+        //    .Take(PageSize));
+
+        public ViewResult List(int page = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
