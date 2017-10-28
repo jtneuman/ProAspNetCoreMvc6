58e165155d5cedbac8b695061c6690f91ecc8726
Jeff <jeff.neumann@phoenix.edu>
2017-10-27 21:29:20 -0400

    Updating routing scheme in StartUp

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -43,11 +43,24 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page{page}",
+                    name: null,
+                    template: "{category}/Page{page:int}",
                     defaults: new { Controller = "Product", action = "List" });
                 routes.MapRoute(
-                    name: "default",
+                    name: null,
+                    template: "Page{page:int}",
+                    defaults: new { Controller = "Product", action = "List", page = 1 });
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { Controller = "Product", action = "List", page = 1 });
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { controller = "Product", action = "List", page = 1 });
+            
+                routes.MapRoute(
+                    name: null,
                     template: "{controller=Product}/{action=List}/{id?}");
             });
             SeedData.EnsurePopulated(app);
