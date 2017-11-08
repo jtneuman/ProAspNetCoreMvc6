796529e859a7e3cf4e9ca9e4544e8edafb40d4d5
Jeff <jeff.neumann@phoenix.edu>
2017-11-07 20:40:20 -0500

    Added IOrderRepository class - next completing the Order controller.

---
+++ SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+namespace SportsStore.Models
+{
+    interface IOrderRepository
+    {
+        IEnumerable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
