ce811d46be4d8bffda76c9d9cd62930bac84fff9
Jeff <jeff.neumann@phoenix.edu>
2017-11-10 21:36:37 -0500

    Updated OrderController to create shipped list and list view.

--- SportsStore/Migrations/20171110022507_ShippedOrders.Designer.cs
+++ SportsStore/Migrations/20171110022507_ShippedOrders.Designer.cs
@@ -8,9 +8,10 @@ using SportsStore.Models;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171110022507_ShippedOrders")]
+    partial class ShippedOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
