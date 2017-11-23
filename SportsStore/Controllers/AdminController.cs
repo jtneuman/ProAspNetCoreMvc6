80139dfebbc0f6b418b0a2828333ee50273ee0d5
Jeff <jeff.neumann@phoenix.edu>
2017-11-22 20:58:44 -0500

    Added Delete method to AdminController.

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -37,7 +37,17 @@ namespace SportsStore.Controllers
             }
         } // end Edit post method
 
-        public ViewResult Create() => View("Edit", new Product());
+        public IActionResult Create() => View("Edit", new Product());
 
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct !=null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
