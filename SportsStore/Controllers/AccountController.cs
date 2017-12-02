223362ecf31bb63ef856e3bbe8721c5900e0dd5a
Jeff <jeff.neumann@phoenix.edu>
2017-12-02 18:41:55 -0500

    Completed the AccountController class.

--- SportsStore/Controllers/AccountController.cs
+++ SportsStore/Controllers/AccountController.cs
@@ -31,6 +31,33 @@ namespace SportsStore.Controllers
                 ReturnUrl = returnUrl
             });
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user =
+                    await userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    if ((await signInManager.PasswordSignInAsync(user,
+                        loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid name or password");
+            return View(loginModel);
+        }// login method
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
 
     }
 }
