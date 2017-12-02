99d8547b32fe670e850f9af72ec54048e04bffc6
Jeff <jeff.neumann@phoenix.edu>
2017-12-01 20:15:34 -0500

    Started AccountController - ended - 11.

---
+++ SportsStore/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userMgr,
+            SignInManager<IdentityUser> signInMgr)
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+        }
+
+        [AllowAnonymous]
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+    }
+}
